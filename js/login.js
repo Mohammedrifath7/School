44281e4084ff884823f16752257a1632e7aecef6
Mohammedrifath7 <rifathjainulla12345@gmail.com>
2025-12-22 20:54:24 +0530

    First Commit- login and dashboard Page

---
+++ js/login.js
@@ -0,0 +1,18 @@
+const userName = "rifath123";
+const password = "rifath_123";
+const submit = document.querySelector('.submit');
+const inputUserName = document.querySelector('.login-user')
+const inputPassword = document.querySelector('.login-password')
+
+submit.addEventListener('click',(event)=>{
+    event.preventDefault();
+    const name = String(inputUserName.value);
+    const pass = String(inputPassword.value);
+    // console.log(name);
+
+    if (name === userName && pass === password) {
+        window.location.href = "../pages/dashboard.html";
+    } else {
+        alert("Invalid credentials");
+    }
+})
